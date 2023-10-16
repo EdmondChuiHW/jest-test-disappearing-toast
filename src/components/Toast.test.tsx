2294f1e1849c81468380ffaa42c573bdceb2f693
Edmond Chui <1967998+EdmondChuiHW@users.noreply.github.com>
2023-10-16 21:02:35 +0100

    Simplify test by removing async

--- src/components/Toast.test.tsx
+++ src/components/Toast.test.tsx
@@ -5,22 +5,18 @@ import "@testing-library/jest-dom";
 beforeEach(() => jest.useFakeTimers());
 afterEach(() => jest.useRealTimers());
 
-it("disappears after 5 seconds", async () => {
+it("disappears after 5 seconds", () => {
   render(<Toast />);
 
-  const textElement = await screen.findByText(
+  const textElement = screen.queryByText(
     "We have been trying to reach out about your extended warranty"
   );
 
   expect(textElement).toBeInTheDocument();
 
-  await act(async () => {
-    await jest.advanceTimersByTimeAsync(4.9 * 1000);
-  });
+  act(() => jest.advanceTimersByTime(4.9 * 1000));
   expect(textElement).toBeInTheDocument();
 
-  await act(async () => {
-    await jest.advanceTimersByTimeAsync(5.1 * 1000);
-  });
+  act(() => jest.advanceTimersByTime(150));
   expect(textElement).not.toBeInTheDocument();
 });
